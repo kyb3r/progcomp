4ea8094a6f6a3b6594111b690b7db2717a27d111
jasonyu0100 <jasonyu0100@gmail.com>
2018-02-20 12:49:29 +1100

    finished moo
    :)

--- 2016 ProgComp/Moo/moo.py
+++ 2016 ProgComp/Moo/moo.py
@@ -1,4 +1,5 @@
 import copy
+import math
 
 def encode(fourDigitNumber):
 	count = 0
@@ -14,7 +15,7 @@ def encode(fourDigitNumber):
 def matcher(codeOne,codeTwo):
 	bulls = ""
 	cows = ""
-	for pos in range(0,10):
+	for pos in range(10):
 		p = codeOne/(8**pos)%8
 		q = codeTwo/(8**pos)%8
 		r = p-q
@@ -26,28 +27,83 @@ def matcher(codeOne,codeTwo):
 				cows += ('C')
 	return bulls+cows
 
-def generateDigitValues(digits):
-	numbers = [[]]
-	for i in range(digits):
+def generateDigitValues(numOfDigits=4):
+	numbers = ['']
+	for i in range(numOfDigits):
 		newNumbers = []
 		for number in numbers:
 			for digit in range(10):
-				if digit in number:
+				if str(digit) in number:
 					continue
 				else:
-					newNumber = copy.deepcopy(number)
-					newNumber.append(digit)
-					newNumbers.append(newNumber)
+					newNumbers.append(str(number) + str(digit))
 		numbers = newNumbers
+
 	return numbers
 
+def selectMiddleNumber(possibilities):
+	length = len(possibilities)
+	midPoint = length/2
+	if midPoint % 2 == 0:
+		#Returns largest of midpoint values from list
+		return possibilities[int(midPoint)]
+	else:
+		#Returns midpoint value from list
+		return possibilities[int(math.ceil(midPoint)) - 1]
+
+def findSimilarity(given, second):
+	if len(given) != len(second):
+		return false
+	count = 0
+	for pos,char in enumerate(given):
+		if char == second[pos]:
+			count += 1
+	return count
+
+def filterList(guess,result,possibilities):
+	necessaryDigits = list(guess)
+	cows = result.count('C')
+	bulls = result.count('B')
+	possibilities = filterCows(guess,cows + bulls,possibilities)
+	possibilities = filterBulls(guess,bulls,possibilities)
+	return possibilities
+def filterBulls(guess,bulls,possibilities):
+	newPossibilites = []
+	for possibility in possibilities:
+		similarity = findSimilarity(guess,possibility)
+		if similarity != bulls:
+			pass
+		else:
+			newPossibilites.append(possibility)
+
+	return newPossibilites
+
+def filterCows(guess,cows,possibilities):
+	newPossibilites = []
+	for possibility in possibilities:
+		digitCount = 0
+		for digit in possibility:
+			if digit in guess:
+				digitCount += 1
+		if digitCount == cows:
+			newPossibilites.append(possibility)
+
+	return newPossibilites
+
+
 def guessSecretNumber(secretNumber):
-	possibilities = generateDigitValues(4)
-	print(len(possibilities))
+	possibilities = generateDigitValues()
+	allowedDigits = {}
+	while True:
+		if len(possibilities) == 1:
+			print(possibilities)
+			break
+		guess = (selectMiddleNumber(possibilities))
+		result = (matcher(encode(guess),secretNumber))
+		possibilities = filterList(guess,result,possibilities)
 
-guessSecretNumber(4)
 
-print(encode('1234'))
 
-print(matcher(620781575,encode('7094')))
+secretNumber = 83916806
+guessSecretNumber(secretNumber)
 
