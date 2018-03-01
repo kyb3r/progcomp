0e8ee8b552d0b636ca1850b2c69849fcdc4e65c1
Jason Yu <jasonyu0100@gmail.com>
2018-03-01 13:48:00 +1100

    Woodchop
    :100:

---
+++ 2014 ProgComp/woodchop.py
@@ -0,0 +1,75 @@
+# def memoize(func):
+# 	cache = func.cache = {}
+# 	def wrapper(*args):
+# 		key = str(args)
+# 		if key not in cache:
+# 			newCost = cost + 0
+# 			cache[key] = {value:func(*args),cost:newCost}
+# 		return cache[key]
+# 	return wrapper
+
+# @memoize
+# def woodCutter(length,*args):
+
+# 	return length,args
+
+# def main(length,cuts):
+# 	#Initialize first cuts
+# 	for cut in cuts:
+# 		woodCutter(cut)
+# 	#Loop through every
+# 	while True:
+# 		cache = woodCutter.cache
+# 		for key in cache:
+
+
+#Solution 1
+
+class Wood:
+	def __init__(self,length,remainingCuts):
+		self.length = length
+		self.remainingCuts = remainingCuts
+		self.sequence = []
+		self.children = [{'start':0,'end':self.length}]
+		self.currentCost = 0
+
+	def cut(self,cutPos):
+		for pos,child in enumerate(self.children):
+			start = child['start']
+			end = child['end']
+			if start < cutPos < end:
+				self.sequence.append(cutPos)
+				self.remainingCuts.remove(cutPos)
+				self.currentCost += abs(end - start)
+				self.children.pop(pos)
+				for newChild in self.generateNewChildren(start,end,cutPos):
+					self.children.append(newChild)
+				break
+		else:
+			raise Exception('No viable cut was found')
+
+	def generateNewChildren(self,start,end,pos):
+		childOne = {'start':start,'end':pos}
+		childTwo = {'start':pos,'end':end}
+		return [childOne,childTwo]
+
+import copy
+def recursion(wood):
+	stack = [wood]
+	while True:
+		wood = stack.pop(0)
+		if len(wood.remainingCuts) == 0:
+			break
+		for cutPos in wood.remainingCuts:
+			newWood = copy.deepcopy(wood)
+			newWood.cut(cutPos)
+			stack.append(newWood)
+	minAmountPile = stack[0]
+	for woodPile in stack:
+		if woodPile.currentCost < minAmountPile.currentCost:
+			minAmountPile = woodPile
+	return minAmountPile
+
+wood = Wood(56.325,[3.9, 11.7, 15.2, 17.2, 19.3, 23.8, 28.6, 31.0, 37.9, 48.3, 48.9, 52.4])
+found = (recursion(wood))
+print(found.sequence,found.currentCost)
